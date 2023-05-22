633fad510271441e69f34813d201f1fbc7f7b994
Andy <al3286957@gmail.com>
2023-05-22 19:09:52 +0300

    Switch to a different css framework pt1 & cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode:'jit',
-  content: ['./views/*.{ejs,js}',
-  '*.{html,js}'],
+  mode: 'jit',
+  content: ["./views/*.ejs"],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
-    },
-    extend: {
-      colors: {
-        greeny:'#14b8a6',
-        bluish:'#ccfbf1'
-      },
-      fontFamily: {
-        navbar:"'Inter Tight', sans-serif",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
+
