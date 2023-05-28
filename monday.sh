92059cf58dd2034b0fd4da9bf400832cb9f74267
Andy <al3286957@gmail.com>
2023-05-28 21:33:33 +0300

    add shell script that will run everything at once

---
+++ monday.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/pi/AndreiAndTheBullshit/innojs/innovationproj
+lxterminal -e "python3 /home/pi/AndreiAndTheBullshit/innojs/innovationproj/PythonScriptPROD.py"
+sudo npm run dev
