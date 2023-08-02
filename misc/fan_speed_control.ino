b6fdd5722c85174009f4a87cb2c4a0b1ba94a7c7
Alissa Šubina <105237164+aalicc@users.noreply.github.com>
2023-08-02 11:39:54 +0300

    add fan speed check

---
+++ misc/fan_speed_control.ino
@@ -0,0 +1,43 @@
+//This code can be used to check HW-095 motor driver or similar. 
+//Type the speed right in the Serial monitor (0 - 100%).
+
+int IN2 = 6;
+int ENA = 5;                                            //PWM signal
+int speed;
+int speed_pct;
+boolean received = false;
+
+void setup() {
+
+Serial.begin(19200);
+
+pinMode(IN2, OUTPUT);
+pinMode(ENA, OUTPUT);                                   //ENA
+digitalWrite(IN2, LOW);                                 // clockwise 
+
+Serial.println("--hello, enter the fan speed in %--");
+}
+
+void loop() {
+EnterSpeed();
+}
+
+
+void EnterSpeed(){
+  
+  if (Serial.available() > 0){
+    speed_pct = Serial.parseInt();                       //reads whole integer in %
+    speed = map(speed_pct, 0, 100, 0, 255);              //convert % to the actual speed value
+  }
+      if ((speed_pct > 0) && (speed_pct <= 100)){        //change to >= 0 if skip print
+      digitalWrite(IN2, HIGH);
+      analogWrite(ENA, speed );
+      Serial.println("current speed = ");
+      Serial.println(speed);                             //prints actual speed
+      delay(3000);
+      }
+        else {
+          digitalWrite(IN2, LOW); 
+          Serial.println("  Fan OFF");
+        }
+}
