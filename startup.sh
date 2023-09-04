7242d03b2826ebcaa7a2c7b8ebace435b7e3e5e2
IoTAndrew <al3286957@gmail.com>
2023-09-04 15:19:38 +0300

    startup script adjustments

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd /home/andreile/farmlab/innovationproj
+lxterminal -e "onboard"
+lxterminal -e "chromium-browser"
 lxterminal -e "python3 /home/andreile/farmlab/innovationproj/cleanRuuvi.py"
 lxterminal -e "python3 /home/andreile/farmlab/innovationproj/PythonScriptPROD.py"
 sudo npm run dev
